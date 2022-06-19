2468bb5629e56f18c4db0a4fa40ed81981f664e6
karmalexz <49818239+karmalexz@users.noreply.github.com>
2022-06-20 04:08:29 +1000

    final project-functional components with hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import CardList from "./components/card-list/card-list.component";
-import SearchBox from "./components/search-box/search-box.component";
-import "./App.css";
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
+
 const App = () => {
-  const [searchField, setSearchField] = useState("");
+  const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
 
-  const filteredMonsters=monsters.filter((monster)=>{
-    return monster.name.toLocaleLowerCase().includes(searchField)
-  })
-
   return (
-    <div className="App">
-      <h1 className="app-title">Monsters-Rolodex</h1>
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+
       <SearchBox
+        className='monsters-search-box'
         onChangeHandler={onSearchChange}
-        placeholder="Search Monsters"
-        className="monster-search-box"
+        placeholder='search monsters'
       />
       <CardList monsters={filteredMonsters} />
     </div>
